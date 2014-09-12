8183a11cd7ce1770f0c5404a2fa7b064e0b8abd3
khdesignverignde <kh@designverign.de>
2014-09-12 17:45:56 +0200

    added plugins: device, vibrate, network-information

--- www/js/index.js
+++ www/js/index.js
@@ -1,25 +1,22 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
+
 var app = {
+    output: {
+        _el: null,
+        showObject: function(obj){
+            var str = '';
+            
+            for(var i in obj){
+                str += i + ': ' + obj[i] + '<br>';
+            }
+            
+            this._el.innerHTML += str + '<br>------------------<br>';
+        }
+    },
+    
     // Application Constructor
     initialize: function() {
         this.bindEvents();
+        this.output._el = document.getElementById('output');
     },
     // Bind Event Listeners
     //
@@ -27,6 +24,9 @@ var app = {
     // 'load', 'deviceready', 'offline', and 'online'.
     bindEvents: function() {
         document.addEventListener('deviceready', this.onDeviceReady, false);
+        document.getElementById('deviceInfoButton').addEventListener('click', this.plugins.deviceInfo, false);
+        document.getElementById('vibrateButton').addEventListener('click', this.plugins.vibrate, false);
+        document.getElementById('showNetworkInfoButton').addEventListener('click', this.plugins.showNetworkInfo, false);
     },
     // deviceready Event Handler
     //
@@ -45,5 +45,21 @@ var app = {
         receivedElement.setAttribute('style', 'display:block;');
 
         console.log('Received Event: ' + id);
+    },
+    plugins:{
+        deviceInfo: function(){
+            console.log(device);
+            app.output.showObject(device);
+        },
+        vibrate: function(){
+            console.log(navigator);
+            app.output.showObject(navigator);
+            navigator.vibrate(1000);
+        },
+        showNetworkInfo: function(){
+            console.log(navigator.network);
+            app.output.showObject(navigator.network);
+            app.output.showObject(navigator.network.connection);
+        }
     }
 };
