3d8d2fdb63a7a91e7965c3ad5907a403c9d7ea70
khdesignverignde <kh@designverign.de>
2014-09-15 11:34:03 +0200

    bs wieder raus2

--- www/js/index.js
+++ www/js/index.js
@@ -28,8 +28,7 @@ var app = {
         document.getElementById('deviceInfoButton').addEventListener('click', this.plugins.deviceInfo, false);
         document.getElementById('vibrateButton').addEventListener('click', this.plugins.vibrate, false);
         document.getElementById('showNetworkInfoButton').addEventListener('click', this.plugins.showNetworkInfo, false);
-        document.getElementById('scanBarcodeButton').addEventListener('click', this.plugins.scanBarcode, false);
-        
+
         document.getElementById('saveToLocalStorageButton').addEventListener('click', this.filetest.saveToLocalStorage, false);
         document.getElementById('loadFromLocalStorageButton').addEventListener('click', this.filetest.loadFromLocalStorage, false);
         
@@ -70,34 +69,6 @@ var app = {
             console.log(navigator.network);
             app.output.showObject(navigator.network);
             app.output.showObject(navigator.network.connection);
-        },
-        scanBarcode:function() {
-//            console.log('scanning');
-//
-//            var scanner = cordova.require("cordova/plugin/BarcodeScanner");
-//
-//            scanner.scan( function (result) { 
-//
-//                alert("We got a barcode\n" + 
-//                "Result: " + result.text + "\n" + 
-//                "Format: " + result.format + "\n" + 
-//                "Cancelled: " + result.cancelled);  
-//
-//               console.log("Scanner result: \n" +
-//                    "text: " + result.text + "\n" +
-//                    "format: " + result.format + "\n" +
-//                    "cancelled: " + result.cancelled + "\n");
-//                document.getElementById("info").innerHTML = result.text;
-//                console.log(result);
-//                /*
-//                if (args.format == "QR_CODE") {
-//                    window.plugins.childBrowser.showWebPage(args.text, { showLocationBar: false });
-//                }
-//                */
-//
-//            }, function (error) { 
-//                console.log("Scanning failed: ", error); 
-//            } );
         }
     },
     filetest:{
